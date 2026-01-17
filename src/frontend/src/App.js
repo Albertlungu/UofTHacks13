c7892494c2247aa381f787b7fd85041db61d8aa9
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2026-01-17 00:08:16 -0500

    feat: added simple camera feed

---
+++ src/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Camera Feed</h1>
+      <img src="http://localhost:5000/video_feed" alt="Camera Feed" />
+    </div>
+  );
+}
+
+export default App;
